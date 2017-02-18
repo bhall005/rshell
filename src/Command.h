a828111c597081a3f80046f0793fe36d71b9a655
vle013 <vle013@ucr.edu>
2017-02-17 17:03:39 -0800

    Update Command.h

--- src/Command.h
+++ src/Command.h
@@ -5,7 +5,10 @@
 
 class Command : public Input {
 public:
+	// Store the command as a string data in the constructor
 	Command(string data) : Input(data) {}
+	
+	// Virtual function execute() that will be implemented in Executable.h and Exit.h
 	virtual bool execute() = 0;
 };
 
