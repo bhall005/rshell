b60718ab4d888ebb85ce6ff689fbb56a2c87d10c
vle013 <vle013@ucr.edu>
2017-02-17 16:45:53 -0800

    Commented Executable.h

--- src/Executable.h
+++ src/Executable.h
@@ -10,62 +10,74 @@ public:
       Executable(string data) : Command(data) {}
       
       bool execute(){
-           // fork();
-           // pid_t pid = getpid();
-	pid_t pid = fork();
+            // Use the syscall fork() to create a child process to execute the command.
+            pid_t pid = fork();
             
-	if(pid < 0) {
-		perror("The forking process failed");
-		return false;
-	}
-
-	if (pid == 0) {
-		char str[data.length()];
-		int words = 1;
-		for(unsigned i = 0; i < data.length(); i++) {
-			str[i] = data.at(i);
-			if(data.at(i) == ' '){
-				words++;
-			}
-		}
-		char * argument;
-		argument = strtok (str, " ");
-		char** arr = new char*[words];
-		unsigned j = 0;
-
-		while (argument != NULL) {
-			arr[j] = new char[strlen(argument)];
-			strcpy(arr[j], argument);
-			j++;
-			argument = strtok(NULL, " ");
-		} 
-                 
-                  if(execvp(arr[0], arr)){
-                        return true;
-                  }
-
-		perror("An error occured while executing an argument");
+            // Checking whether fork() was sucessful.
+            if(pid < 0) {
+                  perror("The forking process failed");
                   return false;
             }
             
-
-	if(pid > 0) {
+            // If in the child process, we will execute the command.
+            if (pid == 0) {
+                  // Convert the string data to a char[]
+                  char str[data.length()];
+                  int words = 1;
+                  for(unsigned i = 0; i < data.length(); i++) {
+                        str[i] = data.at(i);
+                        if(data.at(i) == ' '){
+                              words++;
+                        }
+                  }
+            
+                  // Convert the char[] to a char** for use in execvp()
+                  char * argument;
+                  argument = strtok (str, " ");
+                  char** arr = new char*[words];
+                  unsigned j = 0;
+                  
+                  while (argument != NULL) {
+                        arr[j] = new char[strlen(argument)];
+                        strcpy(arr[j], argument);
+                        j++;
+                        argument = strtok(NULL, " ");
+                  } 
+                  
+                  // Call execvp() to execute the command and return true if successful
+                  if(execvp(arr[0], arr)){
+                        return true;
+                  }
+                  
+                  // If execvp() is not successful, return false
+                  perror("An error occured while executing an argument");
+                  return false;
+            }
+            
+            // If in the parent process, call waitpid()
+            if(pid > 0) {
                   int status;
                   pid_t endPID = waitpid(pid, &status, 0);
-		if(endPID == -1){
-			perror("There was an error calling waitpid on a parent process");
-			return false;
-		}
                   
+                  // Check whether waitpid() was unsuccessful
+                  if(endPID == -1){
+                        perror("There was an error calling waitpid on a parent process");
+                        return false;
+                  }
+                  
+                  // Continue to wait until the child process is completed
                   while (endPID != pid){
+                        // Continue to check whether waitpid() was unsuccessful
                         if(endPID == -1) {
                               perror("There was an error calling waitpid on a parent process");
                               return false;
                         }
                         
+                        // Continue to call waitpid() while waiting
                         endPID = waitpid(pid, &status, 0);
                   }
                   
+                  // Check whether the child process exited normally and return the correct bool value
                   if (WIFEXITED(status)){
                         return true;
                   }
